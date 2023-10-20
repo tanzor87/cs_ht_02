8d74a1ca52caae4aed8fc07754a0bf5db0e0d460
Andrey <rsdak@ksc.krasn.ru>
2023-10-20 10:55:17 +0700

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+int len_number = number.Length;
+
+if (len_number < 3)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> {number[2]}");
+}
+
